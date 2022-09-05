9b0da4680e8d13dba991236ac5214e9b9e6e9ccf
Edson <edsontomasdev@gmail.com>
2022-09-04 21:03:13 -0300

    atualização m3

--- SisTransports/templates/static/js/meusJS/cubagem.js
+++ SisTransports/templates/static/js/meusJS/cubagem.js
@@ -1,11 +1,68 @@
-$('#btnVolumes').on('click', function(e){
-    console.log('volumes')
-    let altura=$('#altura').val().replace(',','.');
-    let largura=$('#largura').val().replace(',','.');
-    let comprimento=$('#comprimento').val().replace(',','.');
-    let volumes=$('#volumes').val().replace(',','.');
-    let totalM3=parseFloat(altura*largura*comprimento*volumes)
-    console.log(totalM3)  
-    $('#totalM3').val(totalM3)    
+$('#btnVolumes').on('click', function(e) {
+    let altura = parseFloat($('#altura').val().replace(",", "."));
+    let largura = parseFloat($('#largura').val().replace(",", "."));
+    let comprimento = $('#comprimento').val().replace(",", ".");
+    let vol = parseFloat($('#vol').val());
+    let memoriaM3 = parseFloat($('#totalM3').val().replace(",", "."))
+    let m3 = altura * largura * comprimento * vol + memoriaM3
+    $('#totalM3').val(m3.toFixed(3))
+    $('#altura').focus()
+    limpaM3();
     e.preventDefault();
 });
+
+$('#btnVolumes').on('blur', function(e) {
+    $('#altura').focus()
+    e.preventDefault();
+});
+
+$('#btnSalvarM3').on('click', function(e) {
+    salvarM3();
+});
+
+$('#totalM3').on('blur', function(e) {
+    console.log('- focus')
+    e.preventDefault();
+});
+
+$('#fechar').on('click', function(e) {
+    descartar();
+    $('#totalM3').val(0)
+
+});
+
+function limpaM3() {
+    $('.m3').val('')
+}
+
+$('#modalCalcM3').on('keypress', function(e) {
+    salvaComTecla(e)
+});
+
+
+function salvarM3() {
+    $('#resultM3').val($('#totalM3').val())
+    limpaM3()
+    $('#totalM3').val(0)
+    $('#modalCalcM3').modal('hide')
+
+}
+
+function descartar() {
+    limpaM3()
+    $('#totalM3').val(0)
+    $('#modalCalcM3').modal('hide')
+
+
+}
+
+
+function salvaComTecla(e) {
+    var tecla = e.keyCode;
+    if (tecla === 115 || tecla === 83) {
+        salvarM3();
+    }
+    if (tecla === 100 || tecla === 68) {
+        descartar();
+    }
+}
