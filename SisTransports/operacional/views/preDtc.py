603def1fa042a184fec4bc3775c6901e236f385c
Edson Tomas <edsontomasdev@gmail.com>
2022-12-16 20:15:30 -0300

    fds

--- SisTransports/operacional/views/preDtc.py
+++ SisTransports/operacional/views/preDtc.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/auth/entrar/')
 def preDtc (request):
     if request.method == 'GET':
-        return render(request, 'preDtc.html')
+        return render(request, 'dtc.html')
     elif request.method == 'POST':
-        return render(request,'preDtc.html')
+        return render(request,'dtc.html')
