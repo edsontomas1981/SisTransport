8e3076dc14ee717d75f67f8381b14c6c20f03b3d
Edson <edsontomasdev@gmail.com>
2023-03-02 22:31:56 -0300

    a

--- SisTransports/SisTransports/settings/base.py
+++ SisTransports/SisTransports/settings/base.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-2l#n&^1q7w%f8n-p-6$vbpuci0b2e_$*y3fu&g)@(uw+r-@s(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
