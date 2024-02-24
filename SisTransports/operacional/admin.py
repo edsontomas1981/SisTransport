5dca1aafacb0e5b9c4133d47fff312504db33c2d
Edson <edsontomasdev@gmail.com>
2024-02-24 11:51:41 -0300

    Atualiza

--- SisTransports/operacional/admin.py
+++ SisTransports/operacional/admin.py
@@ -4,29 +4,11 @@ from operacional.models.marca_veiculos import Marca
 from operacional.models.tipo_carroceria import Tipo_Carroceria
 from operacional.models.tipo_combustivel import Tipo_Combustivel
 from operacional.models.tipo_veiculo import  Tipo_Veiculo
-
+from operacional.models.ocorrencias_manifesto import Ocorrencia_manifesto
 
 admin.site.register(Emissor)
 admin.site.register(Marca)
 admin.site.register(Tipo_Carroceria)
 admin.site.register(Tipo_Combustivel)
 admin.site.register(Tipo_Veiculo)
-
-
-# class ModelosJSON(View):
-#     def get(self, request):
-#         data = {
-#             'marcas': list(Marca.objects.values()),
-#             'tipos_carroceria': list(Tipo_Carroceria.objects.values()),
-#             'tipos_combustivel': list(Tipo_Combustivel.objects.values()),
-#             'tipos_veiculo': list(Tipo_Veiculo.objects.values()),
-#         }
-#         return JsonResponse(data)
-
-
-
-
-
-
-
-
+admin.site.register(Ocorrencia_manifesto)
