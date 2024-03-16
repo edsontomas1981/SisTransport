10fbaff314c40b587ede14687181c6eec186d09f
Edson <edsontomasdev@gmail.com>
2024-03-16 13:40:13 -0300

    arq settings

--- SisTransports/SisTransports/settings/settings.py
+++ SisTransports/SisTransports/settings/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'templates/static'),)
+STATIC_URL = 'home/ubuntu/SisTransports/SisTransports/staticfiles/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'staticfiles'),)
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
