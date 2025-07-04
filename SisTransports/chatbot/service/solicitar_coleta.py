0b4a1f46e527e3dd8749bb31e9902152f043e8c5
Edson Tomas <edsontomasdev@gmail.com>
2025-07-04 15:16:03 -0300

    webhook

--- SisTransports/chatbot/service/solicitar_coleta.py
+++ SisTransports/chatbot/service/solicitar_coleta.py
@@ -28,7 +28,7 @@ def processa_solicitacao_coleta(phone_number, chat, msg):
     )
 
     if campos_preenchidos:
-        return confirma_dados_coleta(phone_number, chat, sufixo),None
+        return confirma_dados_coleta(phone_number, chat, sufixo),{}
 
     if chat["menu"]["passo"] != "aguardando":
         if "_cep" in campo_atual:
