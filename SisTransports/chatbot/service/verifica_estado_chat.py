2cd91aa6cc73c9c81a83689038beb3d582dfe2e2
Edson Tomas <edsontomasdev@gmail.com>
2025-07-04 15:32:38 -0300

    webhook

--- SisTransports/chatbot/service/verifica_estado_chat.py
+++ SisTransports/chatbot/service/verifica_estado_chat.py
@@ -75,7 +75,7 @@ def processar_mensagem(phone_number, mensagem):
 
         resultado = processa_solicitacao_coleta(phone_number, chat, mensagem)
 
-        print(resultado)
+        print(len(resultado))
 
         if isinstance(resultado, tuple):
             pergunta, chat = resultado
